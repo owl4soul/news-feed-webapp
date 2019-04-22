0b4909a94d30b74dc3035e3923cf46904cb5888c
owl4soul <resuuser989@gmail.com>
2019-04-22 17:39:17 +0300

    Test Controller

--- src/main/java/com/github/owl4soul/controllers/MainController.java
+++ src/main/java/com/github/owl4soul/controllers/MainController.java
@@ -1,5 +1,15 @@
 package com.github.owl4soul.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 //Spring controller
+@Controller
 public class MainController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
 }
