814946bbf2387c65df54d5234e2446e0ddcf9bef
owl4soul <resuuser989@gmail.com>
2019-04-24 18:51:13 +0300

    NewsDAO method save()

--- src/main/java/com/github/owl4soul/repository/NewsDAO.java
+++ src/main/java/com/github/owl4soul/repository/NewsDAO.java
@@ -3,11 +3,16 @@ package com.github.owl4soul.repository;
 import com.github.owl4soul.models.News;
 import com.github.owl4soul.utils.HibernateSessionFactoryUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 //Data access object
 public class NewsDAO {
 
     public void save(News news) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory()
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(news);
+        transaction.commit();
+        session.close();
     }
 }
