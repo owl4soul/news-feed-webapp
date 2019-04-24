5314b0085bd0f49bbc627322d45e37f4dac964df
owl4soul <resuuser989@gmail.com>
2019-04-24 18:48:33 +0300

    HibernateSessionFactoryUtil

--- src/main/java/com/github/owl4soul/repository/NewsDAO.java
+++ src/main/java/com/github/owl4soul/repository/NewsDAO.java
@@ -1,7 +1,13 @@
 package com.github.owl4soul.repository;
 
+import com.github.owl4soul.models.News;
+import com.github.owl4soul.utils.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+
 //Data access object
 public class NewsDAO {
 
-    public void save()
+    public void save(News news) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory()
+    }
 }
